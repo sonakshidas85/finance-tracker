7598e19186f457b162cc37c64a856b0612f49024
sonakshidas85 <sonakshidas85@gmail.com>
2026-07-06 11:18:52 +0530

    Initial commit

--- app/src/main/java/com/budgettracker/app/widget/MonthlyBudgetWidgetReceiver.kt
+++
@@ -1,9 +0,0 @@
-package com.budgettracker.app.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-/** AppWidgetProvider entry point for the Monthly widget - see res/xml/monthly_widget_info.xml. */
-class MonthlyBudgetWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = MonthlyBudgetWidget()
-}
